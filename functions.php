35fecc7d46d016d878c804bb1de01ae237de2cce
jbleys <jbleys@uw.edu>
2024-01-23 16:49:20 -0800

    configure wp-dependencies

--- functions.php
+++ functions.php
@@ -1,5 +1,23 @@
 <?php
 /**
+ * WP Dependency Installer
+ * @link: https://github.com/afragen/wp-dependency-installer
+ */
+require_once __DIR__ . '/vendor/autoload.php';
+add_action( 'init', static function() {
+  WP_Dependency_Installer::instance( __DIR__ )->run();
+});
+add_filter(
+  'wp_dependency_dismiss_label',
+  function( $label, $source ) {
+    $label = basename(__DIR__) !== $source ? $label : __( 'Global Health Theme', 'dgh-wp-theme' );
+    return $label;
+  },
+  10,
+  2
+);
+
+/**
  * UW WP Theme Child Theme.
  */
 
