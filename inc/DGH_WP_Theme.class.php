f5c2d3cedb971bdc48ea8b5fb5e3aaaaf224f6b7
jbleys <jbleys@uw.edu>
2024-01-23 21:09:39 -0800

    move option settings to DGH_WP_Theme_Options class file

--- inc/DGH_WP_Theme.class.php
+++ inc/DGH_WP_Theme.class.php
@@ -13,19 +13,11 @@ if ( !class_exists( 'DGH_WP_Theme' ) ) {
 	 */
   class DGH_WP_Theme {
 		
-		/**
-		 * Class variables
-		 */
-		private static $settingsupdated;
-
     /**
     * class constructor
     */
     function __construct()  {
 			
-      // Inititalize settings updated
-      self::$settingsupdated = false;
-
       /**
 			 * implement hook after_setup_theme with priority 11
 			 * that's one priority below the default 10, 
@@ -40,18 +32,15 @@ if ( !class_exists( 'DGH_WP_Theme' ) ) {
 		 */
 		static function dgh_wp_theme_setup() {
 
-			self::_dgh_wp_theme_settings_updated();
+			/**
+			 * options file
+			 */
+			include_once( get_stylesheet_directory() . '/inc/DGH_WP_Theme_Options.class.php' );			
 			
 			/**
 			 * implement hook init
 			 */
 			add_action('init', array( __CLASS__, 'dgh_wp_theme_audience_menu' ) );
-
-			/**
-			 * implement hook admin_init
-			 */
-			add_action('admin_init', array( __CLASS__, 'dgh_wp_theme_add_settings' ) );
-			add_action('admin_init', array( __CLASS__, 'dgh_wp_theme_register_settings' ) );
 
 			/**
 			 * implement hook after_switch_theme
@@ -69,11 +58,6 @@ if ( !class_exists( 'DGH_WP_Theme' ) ) {
 			 * implement hook wp_enqueue_scripts
 			 */
 			add_action( 'wp_enqueue_scripts', array( __CLASS__, 'dgh_wp_theme_enqueue_scripts' ) );
-
-      /**
-			 * implement hook admin_menu
-			 */
-      add_action( 'admin_menu', array( __CLASS__, 'dgh_wp_theme_admin_menu_item' ) );
 			
 			/**
 			 * implement custom hook uw_wp_child_theme_login_link
@@ -85,66 +69,8 @@ if ( !class_exists( 'DGH_WP_Theme' ) ) {
 			 */
 			add_action( 'wp_dashboard_setup', array( __CLASS__, 'dgh_wp_theme_dashboard_setup' ), 11 );
 
-			/**
-			 * implement hook wp_ajax_{action}
-			 */
-			add_action( 'wp_ajax_enable_card_title_attribute_ajax_callback', array( __CLASS__, 'enable_card_title_attribute_ajax_callback' ) );
-			add_action( 'wp_ajax_nopriv_enable_card_title_attribute_ajax_callback', array( __CLASS__, 'enable_card_title_attribute_ajax_callback' ) );
-
 		}
 		
-    /**
-    * Is Postback from submitting the option form
-    */
-    private static function _dgh_wp_theme_settings_updated() {
-
-      if( isset( $_GET['settings-updated'] ) ){
-
-        self::$settingsupdated = $_GET['settings-updated'];
-
-      }
-
-			return self::$settingsupdated;
-
-    }
-    /**
-     * Callback for hook admin_init
-		 * 
-		 * Add settings sections/fields
-     */
-		static function dgh_wp_theme_add_settings() {
-
-			add_settings_section( 
-				'dgh-wp-theme-options-cards',	//Slug-name to identify the section
-				__('Cards settings', 'dgh-wp-theme'),	//Formatted title of the section
-				array( __CLASS__, 'dgh_wp_theme_options_section_cards' ),	//Function that echos out any content at the top of the section (between heading and fields)
-				'dgh-wp-theme-options',	//The slug-name of the settings page on which to show the section
-				array(
-					'before_section' => '<section>',
-					'after_section ' => '</section>',
-					'section_class  ' => 'dgh-wp-theme-options-cards',
-				)	//Arguments used to create the settings section
-			);
-
-			// setting field to enable the title attribute in card
-			add_settings_field(
-				'dgh-wp-theme_enable_card_title_attribute',	//Slug-name to identify the field. Used in the 'id' attribute of tags
-				__('Set the title attribute', 'dgh-wp-theme'),	//Formatted title of the field. Shown as the label for the field during output
-				array( __CLASS__, 'dgh_wp_theme_enable_card_title_attribute_callback' ),	//Callback Function that fills the field with the desired form inputs. The function should echo its output
-				'dgh-wp-theme-options',	//The slug-name of the settings page
-				'dgh-wp-theme-options-cards',	//The slug-name of the section of the settings page in which to show the box
-				array(
-					'label_for' => 'dgh-wp-theme_enable_card_title_attribute',
-					'default_value' => 0,
-					'type' => 'checkbox',
-					'id' => 'dgh-wp-theme_enable_card_title_attribute',
-					'name' => 'dgh-wp-theme_enable_card_title_attribute',
-					'description' => "Sets the title attribute of the element with the .card class using the text of the element with the .card-title class."
-				)	//Extra arguments that get passed to the callback function
-			);
-
-		}
-
 		/**
 		 * Callback function for hook after_switch_theme
 		 * Fires once after theme activation
@@ -159,21 +85,6 @@ if ( !class_exists( 'DGH_WP_Theme' ) ) {
 		 */
 		static function dgh_wp_theme_switch_theme( $new_name, $new_theme, $old_theme ) {
 			// nothing to do here
-		}
-
-		/**
-		 * Callback for hook admin_init
-		 * Register theme settings
-		 */
-		static function dgh_wp_theme_register_settings() {
-			
-			// Option to enable the title attribute on card elements
-			register_setting(
-				"dgh-wp-theme_options",		//settings group name
-				"dgh-wp-theme_enable_card_title_attribute",		//name of an option to sanitize and save
-				array('default' => 0,)		//Data used to describe the setting when registered
-			);
-
 		}
 
 		/**
@@ -202,76 +113,6 @@ if ( !class_exists( 'DGH_WP_Theme' ) ) {
 			$UW_WP_Child_Theme = array( 'admin_ajax_url' => admin_url('admin-ajax.php') );
 			wp_localize_script( 'dgh-wp-theme', 'UW_WP_Child_Theme', $UW_WP_Child_Theme );
 
-		}
-
-    /**
-    * Callback function for hook admin_menu
-    */
-    static function dgh_wp_theme_admin_menu_item() {
-							
-			// Add a submenu page to the UW theme's main menu item.
-			add_submenu_page(
-				'uw-theme-settings',		//The slug name for the parent menu (or the file name of a standard WordPress admin page).
-				__('Global Health Theme Settings', 'dgh-wp-theme'),	//The text to be displayed in the title tags of the page when the menu is selected.
-				__('Global Health Theme Settings', 'dgh-wp-theme'),	//The text to be used for the menu.
-				'manage_options',				//The capability required for this menu to be displayed to the user.
-				'dgh-wp-theme-options',	//The slug name to refer to this menu by.
-				array( __CLASS__, 'dgh_wp_theme_options_page' )	//The function to be called to output the content for this page.
-			);
-
-    }
-		
-		/**
-		 * Callback function for settings section dgh-wp-theme-options-cards output
-		 */
-		static function dgh_wp_theme_options_section_cards( $args ) {
-			?>
-			<p><?php _e('Here you can manage options for the Cards.', 'dgh-wp-theme'); ?></p>
-			<?php
-		}
-
-		/**
-		 * Callback function for setting dgh-wp-theme_enable_card_title_attribute output
-		 */
-		static function dgh_wp_theme_enable_card_title_attribute_callback( $args ) {
-			?>
-				<input type="<?php echo esc_attr( $args['type'] ); ?>" id="<?php echo esc_attr( $args['id'] ); ?>"  name="<?php echo esc_attr( $args['name'] ); ?>"  value="1" <?php checked(1, get_option('dgh-wp-theme_enable_card_title_attribute'), true); ?> />
-				<p class="description"><?php echo esc_attr( $args['description'] ); ?></p>
-			<?php
-		}
-
-    /**
-    * Callback function to create the output for the options page.
-    */
-    static function dgh_wp_theme_options_page() {
-			
-			if ( !current_user_can('manage_options') )
-				return;
-
-			?>
-			<div class="wrap">
-				<h1><?php _e('Global Health Theme Options','dgh-wp-theme');?></h1>
-				<?php settings_errors(); ?>
-				<form action="options.php" method="post" id="dgh-wp-theme-options-form">
-					<?php 
-					settings_fields('dgh-wp-theme_options');
-					do_settings_sections( 'dgh-wp-theme-options' );	//The slug name of the page
-					submit_button( __('Save Settings', 'dgh-wp-theme') ); 
-					?>
-				</form>
-			</div>
-			<?php 
-		}
-
-		/**
-		 * Callback for hook wp_ajax_{action}
-		 */
-		static function enable_card_title_attribute_ajax_callback() {
-
-			echo get_option('dgh-wp-theme_enable_card_title_attribute');
-			
-			// Don't forget to stop execution afterward.
-			wp_die();
 		}
 
     /**
