436544ac1b6e6600ee8b24e80f1ae302321f5cb7
jbleys <jbleys@uw.edu>
2024-01-25 23:05:08 -0800

    comment cleanup

--- functions.php
+++ functions.php
@@ -18,14 +18,11 @@ add_filter(
 );
 
 /**
- * UW WP Theme Child Theme.
+ * UW WP Theme Child Theme functionality
  */
-
 require( get_stylesheet_directory() . '/inc/DGH_WP_Theme.class.php' );
 
 /**
- * uw_wp_theme overrides
- * 
- * Functions which enhance the theme by hooking into WordPress.
+ * uw_wp_theme template-functions.php overrides
  */
 require get_stylesheet_directory() . '/inc/template-functions.php';
