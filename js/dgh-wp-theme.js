6aab027a59b036178ae0c9b277811701ae4d23d7
jbleys <jbleys@uw.edu>
2024-02-24 15:32:16 -0800

    dynamically resize the height of mobile sticky menu making it vertically scrollable within the window

--- js/dgh-wp-theme.js
+++ js/dgh-wp-theme.js
@@ -25,12 +25,25 @@
 			});
 
 		},
+		// member function
+		sticky_header_menu_resize() {
+			var maxheight = window.innerHeight - 75;	// 75 = 45(navbar height) + 30(buffer)
+			if ( parseInt($(window).width()) < 768 ) {
+				$(" div[id^='uw-wp-child-theme-sticky-header'] .navbar .container-fluid ").css({"max-height": maxheight+"px", "overflow-y": "scroll", "overflow-x": "clip"});
+			} else {
+				$(" div[id^='uw-wp-child-theme-sticky-header'] .navbar .container-fluid ").css({"max-height": "", "overflow-y": "", "overflow-x": ""});
+			}
+		},
 
 	};
 
 	// The document ready event executes when the HTML-Document is loaded
 	// and the DOM is ready.
 	$(document).ready(function( $ ) {
+		
+    $(window).on("scroll resize click", function(){
+      uw_wp_child_theme_options.sticky_header_menu_resize();
+    });
 		
 		uw_wp_child_theme_options.do_card_title_attribute();
 		
