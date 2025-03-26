01ba021b97e897ef7718f358f67ee7aabcad48c1
jbleys <jbleys@uw.edu>
2025-03-25 23:34:09 -0700

    add script file for faculty landing page

---
+++ js/dgh-wp-theme-page-faculty.js
@@ -0,0 +1,31 @@
+/**
+ * jQuery should be accessed through $ by passing the jQuery object into an anonymous function.
+ * @see {@link https://developer.wordpress.org/coding-standards/wordpress-coding-standards/javascript/#common-libraries}
+ */
+(function ($, window, document, undefined) {
+	// At the top of the file, set "wp" to its existing value (if present)
+	window.wp = window.wp || {};
+
+	// The document ready event executes when the HTML-Document is loaded
+	// and the DOM is ready.
+	$(document).ready(function( $ ) {
+
+		//a11y: add tabindex attr to pagination button links
+		$( 'a[id^=btn-faculty-page-], #btn-faculty-view-all' ).attr( 'tabindex', "0" );
+		//a11y: add list role to the cards grid
+		$( '#faculty-cards' )
+			.attr( 'role', 'list' )
+			.attr( 'aria-describedby', 'faculty-cards-description');
+		//a11y: add listitem role to each su card container
+		$( '.su-post-faculty-card' )
+			.attr( 'role', 'listitem' );
+		
+	});
+
+	// The window load event executes after the document ready event,
+	// when the complete page is fully loaded.
+	$(window).on('load', function () {
+		// nothing to do
+	});
+	
+})(jQuery, this, this.document);
