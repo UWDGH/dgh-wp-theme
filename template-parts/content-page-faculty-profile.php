af7c3dcfc034f0eea63f3a8a0999abddbac527be
jbleys <jbleys@uw.edu>
2025-03-31 17:29:29 -0700

    template part code cleanup

--- template-parts/content-page-faculty-profile.php
+++ template-parts/content-page-faculty-profile.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Template part for displaying page content in page.php
+ * Template part for displaying page content in template-faculty-profile.php
  *
  * @link https://codex.wordpress.org/Template_Hierarchy
  *
@@ -10,217 +10,197 @@
 ?>
 
 <?php
-// get the data
+/**
+ * variable declarations
+ */
+// post and meta variables
+$id = '';					// Post id
+$title = '';				// Post title
+$fac_bio = '';				// Post content (i.e. Faculty Bio)
+$fac_image_url = '';		// feaured image
+$fac_image_alt_text = '';	// alt text (appears unused in UW's 'large' card)
+$fac_appts = array();		// UW appointments
+$fac_job_title = array();	// Additional job title(s)
+$fac_fname = '';			// first name
+$fac_lname = '';			// last name
+$fac_appt_ttl_dept = '';	// that one strange appointment title field. we can use it as a fallback
+$fac_phone_number = '';		// phone number
+$fac_phone_number_hidden = false;
+$fac_office = '';			// office location
+$fac_office_hidden = false;
+$fac_image_url = get_stylesheet_directory_uri() . '/assets/img/profile-placeholder.png';
+
+// output(i.e. print) vars are prefixed with 'p'
+$p_fac_bio = '';
+$p_fac_appts = '';
+$p_fac_research_interests = '';
+$p_fac_education = '';
+$p_fac_contact = '';
+$p_fac_email = '';
+$p_fac_phone_number = '';
+$p_fac_office = '';
+
+// content headings vars are prefixed with 'h'
+$h_bio = '<h2>'.__('Bio', 'dgh-wp-theme').'</h2>';
+$h_education = '<h2>'.__('Education', 'dgh-wp-theme').'</h2>';
+$h_contact = '<h2>'.__('Contact Information', 'dgh-wp-theme').'</h2>';
+$h_research_interests = '<h2>'.__('Areas of Expertise', 'dgh-wp-theme').'</h2>';
+$h_publications = '<h2>'.__('Publications', 'dgh-wp-theme').'</h2>';
+
+/**
+ * retrieve post data
+ */
+$id = $post->ID;
 $title = get_the_title();
-
-// image
-$image_url = get_stylesheet_directory_uri() . '/assets/img/profile-placeholder.png';
-$alt_text = '';
-if ( has_post_thumbnail( $post->ID ) ) {
-	$image_url = get_the_post_thumbnail_url( $post->ID );
-	$att_id = attachment_url_to_postid( $image_url );
-	$alt_text = wp_get_attachment_caption( $att_id );
-}
-// appointments
-$appts =  get_post_meta( $post->ID, '_dgh_fac_appts' );
-if ( !empty( $appts ) ) { $appts = $appts[0]; }
+$fac_bio = apply_filters( 'the_content', get_the_content() );
+$fac_research_interests = get_post_meta( $id, '_dgh_fac_research_interests', true );
+$fac_fname = get_post_meta( $id, '_dgh_fac_fname', true );
+$fac_lname = get_post_meta( $id, '_dgh_fac_lname', true );
+$fac_email =  get_post_meta( $id, '_dgh_fac_email', true );
+$fac_phone_number =  get_post_meta( $id, '_dgh_fac_phone_number', true );
+$fac_phone_number_hidden =  get_post_meta( $id, '_dgh_fac_phone_number_hidden', true );
+$fac_office =  get_post_meta( $id, '_dgh_fac_office', true );
+$fac_office_hidden =  get_post_meta( $id, '_dgh_fac_office_hidden', true );
+$fac_appt_ttl_dept = get_post_meta( $id, '_dgh_fac_appt_ttl_dept', true );
+// appts
+$fac_appts =  get_post_meta( $id, '_dgh_fac_appts' );
+if ( !empty( $fac_appts ) ) { $fac_appts = $fac_appts[0]; }
 // job titles
-$job_title =  get_post_meta( $post->ID, '_dgh_fac_job_title' );
-if ( !empty( $job_title ) ) { $job_title = $job_title[0]; }
-// that one strange title field
-$fac_appt_ttl_dept = get_post_meta( $post->ID, '_dgh_fac_appt_ttl_dept', true );
-do_action('qm/debug', empty($fac_appt_ttl_dept) );
-
-// email
-$fac_email =  get_post_meta( $post->ID, '_dgh_fac_email', true );
-$card_email = '';
-if ( $fac_email ) {
-	$card_email  = '<div class="fac-email">';
-	$card_email .= '<p><a href="'.esc_url( 'mailto:' . $fac_email ).'">'.esc_html( $fac_email ).'</a></p>';
-	$card_email .= '</div>';
+$fac_job_title =  get_post_meta( $id, '_dgh_fac_job_title' );
+if ( !empty( $fac_job_title ) ) { $fac_job_title = $fac_job_title[0]; }
+// featured image
+if ( has_post_thumbnail( $id ) ) {
+	$fac_image_url = get_the_post_thumbnail_url( $id );
+	$att_id = attachment_url_to_postid( $fac_image_url );
+	$fac_image_alt_text = wp_get_attachment_caption( $att_id );
 }
 
-// construct the fac-appts div
-$fac_appts = '';
-if ( !empty( $appts) || !empty( $job_title)  ) {
-	$fac_appts .= '<div class="fac-appts">';
-	if ( !empty( $appts) ) {
-		foreach ($appts as $key => $value) {
-			$fac_appts .= '<div role="note" aria-label="University of Washington Appointment">' . $value . '</div>';
+/**
+ * construct output vars
+ */
+// construct appointments
+if ( !empty( $fac_appts) || !empty( $fac_job_title)  ) {
+	$p_fac_appts .= '<div class="fac-appts">';
+	if ( !empty( $fac_appts) ) {
+		foreach ($fac_appts as $key => $value) {
+			$p_fac_appts .= '<div role="note" aria-label="'.__('University of Washington Appointment','dgh-wp-theme').'">' . $value . '</div>';
 		}
 	}
-	if ( !empty( $job_title) ) {
-		foreach ($job_title as $key => $value) {
-			$fac_appts .= '<div role="note" aria-label="Job Title">' . $value . '</div>';
+	if ( !empty( $fac_job_title) ) {
+		foreach ($fac_job_title as $key => $value) {
+			$p_fac_appts .= '<div role="note" aria-label="'.__('Job Title','dgh-wp-theme').'">' . $value . '</div>';
 		}
 	}
-	$fac_appts .= '</div>';
+	$p_fac_appts .= '</div>';
 } elseif ( !empty($fac_appt_ttl_dept) ) {
-	$fac_appts .= '<div class="fac-appts">';
-	$fac_appts .= '<div role="note" aria-label="Department Title">' . $fac_appt_ttl_dept . '</div>';
-	$fac_appts .= '</div>';
+	// fallback appt title
+	$p_fac_appts .= '<div class="fac-appts">';
+	$p_fac_appts .= '<div role="note" aria-label="'.__('Department Appointment Title','dgh-wp-theme').'">' . $fac_appt_ttl_dept . '</div>';
+	$p_fac_appts .= '</div>';
 }
 
-// add the fac-appts div to the body of the card
-$card_body = '';
-$card_body .= $fac_appts;
+// construct email
+if ( $fac_email ) {
+	$p_fac_email = '<div class="fac-email" data-alt="Email">';
+	$p_fac_email .= '<span class="dashicons dashicons-email"><span class="screen-reader-text">'.__('Email','dgh-wp-theme').'</span></span>';
+	$p_fac_email .= '<a href="'.esc_url( 'mailto:' . $fac_email ).'">'.esc_html( $fac_email ).'</a>';
+	$p_fac_email .= '</div>';
+}
 
-do_action('qm/debug', $post );
-do_action('qm/debug', get_post_meta( $post->ID ) );
+// construct phone number
+if ( $fac_phone_number ) {
+	$p_fac_phone_number = '<div class="fac-phone" data-alt="Phone number">';
+	$p_fac_phone_number .= '<span class="dashicons dashicons-phone"><span class="screen-reader-text">'.__('Phone number','dgh-wp-theme').'</span></span>';
+	$p_fac_phone_number .= ($fac_phone_number_hidden) ? __('N/A','dgh-wp-theme') : $fac_phone_number;
+	$p_fac_phone_number .= '</div>';
+}
+
+// construct office location
+if ( $fac_office ) {
+	$p_fac_office = '<div class="fac-office" data-alt="Office location">';
+	$p_fac_office .= '<span class="dashicons dashicons-location"><span class="screen-reader-text">'.__('Office location','dgh-wp-theme').'</span></span>';
+	$p_fac_office .= ($fac_office_hidden) ? __('N/A','dgh-wp-theme') : wpautop( $fac_office );
+	$p_fac_office .= '</div>';
+}
+
+// construct contact
+if ( empty($fac_office) && empty($fac_email) && empty($fac_phone_number) ) {
+	$p_fac_contact = $h_contact . 'N/A';
+} else {
+	$p_fac_contact = $h_contact . $p_fac_office . $p_fac_phone_number . $p_fac_email;
+}
+
+// construct bio
+$p_fac_bio = ( !empty( $fac_bio ) ) ? $h_bio . $fac_bio : $h_bio . 'N/A';
+
+// construct areas of expertise
+$p_fac_research_interests = ( !empty( $fac_research_interests ) ) ? $h_research_interests . wpautop( $fac_research_interests ) : $h_research_interests . 'N/A';
+
 ?>
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 
 	<div class="entry-content entry-content-faculty-profile">
 
-	<?php 
-	// construct the card
-	$uw_card = <<<UW_CARD
-	<style>
-	.card.large .image-large {
-		background-position: 0 0;	}
-	.card.large .card-body {
-		min-height: 360px; }
-	</style>
-	[uw_card 
-	style="large" 
-	align="left" 
-	color="gold" 
-	title="{$title}" 
-	titletag="h1" 
-	image="{$image_url}" 
-	alt="{$alt_text}"]
-	<div class="udub-slant-divider"><span></span></div>
-	{$card_body}
-	{$card_email}
-	[/uw_card]
-	UW_CARD;
+		<h1 class="entry-title screen-reader-text" aria-hidden="true"><?php the_title(); ?></h1> <!--//hidden from accessibility API. Title is duplicated in the card title//-->
 
-	// display the card
-	echo do_shortcode( $uw_card ); 
-
-	/*
-	?>
-
-		<h1 class="entry-title"><?php the_title(); ?></h1>
-
-		<?php if ( !empty( $appts) || !empty( $job_title)  ) : ?>
-		<div class="fac-appts">
-			<?php if ( !empty( $appts) ) : ?>
-				<?php foreach ($appts as $key => $value) : ?>
-					<div><?php //echo $value; ?></div>
-				<?php endforeach; ?>
-			<?php endif; ?>
-			<?php if ( !empty( $job_title) ) : ?>
-				<?php foreach ($job_title as $key => $value) : ?>
-					<div><?php //echo $value; ?></div>
-				<?php endforeach; ?>
-			<?php endif; ?>
-		</div>
-		<?php endif; ?>
-		<?php
-	*/
-			// TO DO: design presentation
-			/*
-			if ( has_post_thumbnail( get_the_ID() ) ) {
-				$url_full  = wp_get_attachment_image_src( get_post_thumbnail_id(), 'full');
-				?>
-				<div id="featured-image-<?php the_ID(); ?>" class="featured-image entry-thumbnail dgh-faculty-profile-image">
-					<a href="<?php echo $url_full[0]; ?>" title="This link displays a full size photo of <?php echo get_the_title(); ?>.">
-					<?php
-					the_post_thumbnail( 'medium' , array('id' => 'wp-post-image-'.get_the_ID(), 'class' => 'alignright wp-post-image--faculty-profile wp-img' ) );
-					?>
-					<span class="screen-reader-text"> A headshot photo of <?php echo get_the_title(); ?></span>
-					</a>
-				</div>
-				<?php
-			} else {
-				?>
-				<div id="featured-image-placeholder" class="featured-image entry-thumbnail dgh-faculty-profile-image">
-					<img src="<?php echo get_stylesheet_directory_uri() . '/assets/img/profile-placeholder.png'; ?>" id="wp-post-image-placeholder" class="alignright wp-post-image-placeholder wp-img" decoding="async" fetchpriority="high" width="300" height="300">
-					<span class="screen-reader-text"> A profile placeholder image.</span>
-				</div>
-				<?php
-			}
-				*/
-		?>
 		<div class="entry-content-wrapper">
-			<!-- <h2>Bio</h2> -->
-		<?php
+
+			<section aria-label="<?php _e( 'Faculty profile header', 'dgh-wp-theme' ); ?>" aria-describedby="faculty-profile-header-description">
+				
+				<p id="faculty-profile-header-description" class="screen-reader-text"><?php _e( 'Above the fold content, presented visually as a card. Showing a faculty profile image on one side of the card, and their name, appointment titles, and email address on the other side of the card.', 'dgh-wp-theme' ); ?></p>
+			
+				<?php
+				// construct the card shortcode with appointments and email as the body
+				$uw_card = <<<UW_CARD
+				[uw_card style="large" align="left" color="gold" title="{$title}" titletag="h1" image="{$fac_image_url}" alt="{$fac_image_alt_text}"]
+				<div class="udub-slant-divider"><span></span></div>
+				{$p_fac_appts}
+				{$p_fac_email}
+				[/uw_card]
+				UW_CARD;
+			
+				// display the card
+				echo do_shortcode( $uw_card ); 
+
+				?>
+			</section>
+			<?php
 			// the_content();
 
+			// construct the 'About' tabs section
 			$tabs_section_about = '';
-			$fac_bio = apply_filters( 'the_content', get_the_content() );
-			$fac_fname = get_post_meta( $post->ID, '_dgh_fac_fname', true );
-			$fac_lname = get_post_meta( $post->ID, '_dgh_fac_lname', true );
-			
-			$fac_education = '';
-			if ( !empty($fac_bio) ){
-				$tabs_section_about = <<<TAB_ABOUT
-				[tabs_section title="About {$fac_fname} {$fac_lname}" id="tab-fac-about"]
-				[row]
-				[col class="col-sm col-sm-12 col-xl-8"]
-				<h2>Bio</h2>
-				<p>{$fac_bio}</p>
-				[/col]
-				[col class="col-sm col-sm-12 col-xl-4 pl-0 pl-xl-auto"]
-				<h2>Education</h2>
-				<p>{$fac_education}</p>
-				<p>
-					<div>degrees[deg_ttl], degrees[inst_ttl]</div>
-					<div>degrees[deg_ttl], degrees[inst_ttl]</div>
-					<div>degrees[deg_ttl], degrees[inst_ttl]</div>
-				</p>
-				[/col]
-				[/row]
-				[/tabs_section]
-				TAB_ABOUT;
-			}
+			$tabs_section_about = <<<TAB_ABOUT
+			[tabs_section title="About {$fac_fname} {$fac_lname}" id="tab-fac-about"]
+			[row]
+			[col class="col-sm col-sm-12 col-xl-8"]
+			{$p_fac_bio}
+			{$p_fac_research_interests}
+			[/col]
+			[col class="col-sm col-sm-12 col-xl-4 pl-0 pl-xl-auto"]
+			{$h_education}
+			<p>{$p_fac_education}</p>
+			<p>
+				<div>degrees[[deg_ttl]], degrees[[inst_ttl]]</div>
+				<div>degrees[[deg_ttl]], degrees[[inst_ttl]]</div>
+				<div>degrees[[deg_ttl]], degrees[[inst_ttl]]</div>
+			</p>
+			{$p_fac_contact}
+			[/col]
+			[/row]
+			[/tabs_section]
+			TAB_ABOUT;
 
-			$tab_contact = <<<TAB_CONTACT
-			TAB_CONTACT;
-			
-			$fac_research_interests = get_post_meta( $post->ID, '_dgh_fac_research_interests', true );
-			// do_action('qm/debug', empty($fac_research_interests) );
-			$tab_expertise = '';
-			if ( !empty($fac_research_interests) ){
-				$tab_expertise = <<<TAB_EXPERTISE
-				[tabs_section title="Areas of Expertise" id="tab-fac-expertise"]
-				<h2>Areas of Expertise</h2>
-				<p>{$fac_research_interests}</p>
-				[/tabs_section]
-				TAB_EXPERTISE;
-			}
-
-			$id = get_the_ID();
 			$fac_tabs = <<<FAC_TABS
-			[uw_tabs id="tabs-faculty-{$id}" style="alt-tab"]
+			[uw_tabs id="tabs-fac-{$id}" style="alt-tab"]
 			{$tabs_section_about}
-			[tabs_section title="Contact Information" id="tab-fac-contact"]
-				<h2>Contact Information</h2>
-				<p>
-					<div>contact[street_addr]</div>
-					<div>contact[street_addr_2]</div>
-					<div>contact[street_addr_3]</div>
-					<div>contact[campus_box]</div>
-					<div>contact[city] contact[state] contact[zip]</div>
-				</p>
-				<p>
-					<div>contact[phone_number]</div>
-				</p>
-				<p>
-					<div>contact[email]</div>
-				</p>
-			[/tabs_section]
-			{$tab_expertise}
-			[tabs_section title="Primary Research Area" id="tab-fac-research"]
-
-			[/tabs_section]
 			[tabs_section title="Publications" id="tab-fac-publications"]
 				<h2>Publications</h2>
-				<p>publications[fac_web_text]</p>
+				<p>publications[[fac_web_text]]</p>
 			[/tabs_section]
 			[/uw_tabs] 
 			FAC_TABS;
+
+			// display the tabs
 			echo do_shortcode( $fac_tabs );
 			
 			wp_link_pages(
@@ -229,7 +209,8 @@ do_action('qm/debug', get_post_meta( $post->ID ) );
 					'after'  => '</div>',
 				)
 			);
-		?>
+			?>
+			
 		</div>
 
 		<?php if ( get_edit_post_link() ) : ?>
