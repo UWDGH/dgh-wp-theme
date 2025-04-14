0e1c9cc27a30d45e18ec20edc4d254dd0b9efee1
jbleys <jbleys@uw.edu>
2025-04-14 13:13:32 -0700

    align modal thumbnail right

--- su-posts-templates/faculty-card-loop.php
+++ su-posts-templates/faculty-card-loop.php
@@ -71,7 +71,7 @@
 				$image_url = get_the_post_thumbnail_url( $the_ID );
 				$att_id = attachment_url_to_postid( $image_url );
 				$alt_text = wp_get_attachment_caption( $att_id );
-				$modal_thumbnail = get_the_post_thumbnail( $post, 'thumbnail', array( 'class' => 'alignleft' ) );
+				$modal_thumbnail = get_the_post_thumbnail( $post, 'thumbnail', array( 'class' => 'alignright' ) );
 			} elseif ( $photo_url ) {
 				// fallback: deeplink the SPH photo URL
 				if ( DGH_Template::is_dgh_fac_photo_url_ok( $photo_url ) ) {
